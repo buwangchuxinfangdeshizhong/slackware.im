35dfa3e5b068a7c17d2dc5967bc407c59cc675c0
saharabear <iam@saharabear.com>
2013-06-30 02:41:46 +0800

    add download part

---
+++ src/Slackiss/Bundle/SlackwareBundle/Controller/DownloadController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Slackiss\Bundle\SlackwareBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/download")
+ */
+class DownloadController extends Controller
+{
+    /**
+     * @Route("/",name="download")
+     * @Template()
+     * @Method({"GET"})
+     */
+    public function indexAction(Request $request)
+    {
+        $param=array('nav_active'=>'nav_active_download');
+        return $param;
+    }
+}
+
