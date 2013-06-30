3265af289effbd1f3ddf48af9afadf9c3d1f1c51
saharabear <iam@saharabear.com>
2013-06-30 10:39:00 +0800

    add ChangeLog and Package page

---
+++ src/Slackiss/Bundle/SlackwareBundle/Controller/PackageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Slackiss\Bundle\SlackwareBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/package")
+ */
+class PackageController extends Controller
+{
+    /**
+     * @Route("/",name="package")
+     * @Template()
+     * @Method({"GET"})
+     */
+    public function indexAction(Request $request)
+    {
+        $param=array('nav_active'=>'nav_active_package');
+        return $param;
+    }
+}
+
