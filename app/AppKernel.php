afdd4b299ef081316340a11def5fe2e9cdb4c9b1
Haulyn Jason <iam@saharabear.com>
2014-09-07 23:35:21 +0800

    add HWIOAuthBundle routing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             //new SimpleThings\FormExtraBundle\SimpleThingsFormExtraBundle(),
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new Slackend\Bundle\TwigExtraBundle\SlackendTwigExtraBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
