d05a70de46f7fc7d2440895976b39c25b6561d9e
Haulyn Jason <iam@saharabear.com>
2014-09-25 11:47:28 +0800

    add username/password valid

--- src/Slackiss/Bundle/SlackwareBundle/Controller/AccountController.php
+++ src/Slackiss/Bundle/SlackwareBundle/Controller/AccountController.php
@@ -47,23 +47,57 @@ class AccountController extends Controller
         $form = $this->getForm(['username'=>$current->getUsername(),
                                'email'   =>$current->getEmail()
         ]);
+        $param['form']=$form->createView();
         $form->handleRequest($request);
         $userManager = $this->get('fos_user.user_manager');
         $data = $form->getData();
-        if($form->isValid()
-        &&!$userManager->findUserByUsername(trim($data['username']))
-        &&!$userManager->findUserByEmail(trim($data['email']))
-        ){
+        $username = $data['username']?trim($data['username']):'';
+        $email    = $data['email']?trim($data['email']):'';
+
+        if(!$username||!$email){
+            $this->get('session')->getFlashBag()
+                 ->add('error','请输入用户名和电子信箱');
+            return $param;
+        }
+
+        if(strlen($username)>36||strlen($username)<4||!filter_var($username,FILTER_VALIDATE_REGEXP,['options'=>['regexp'=>"/^[A-z0-9]*$/i"]])){
+            $this->get('session')->getFlashBag()
+                 ->add('error','用户名必须是4-36位的英文或者数字');
+            return $param;
+        }
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $this->get('session')->getFlashBag()
+                 ->add('error','请输入合法的电子信箱');
+            return $param;
+        }
+
+        if($userManager->findUserByUsername($username)){
+            if($current->getUsername()!==$username){
+                $this->get('session')->getFlashBag()
+                     ->add('error','用户名已被占用');
+                return $param;
+            }
+        }
+
+        if($userManager->findUserByEmail($email)){
+            if($current->getEmail()!==$email){
+                $this->get('session')->getFlashBag()
+                     ->add('error','电子信箱已被占用');
+                return $param;
+            }
+        }
+
+        if($form->isValid()){
             $current->setUsername(trim($data['username']));
             $current->setEmail(trim($data['email']));
             $userManager->updateUser($current);
             $this->get('session')->getFlashBag()->add('success','保存成功');
             return $this->redirect($this->generateUrl('member_account'));
         }else{
-            $this->get('session')->getFlashBag()->add('danger','请使用4-36个英文字母或数字作为用户名,同时请使用网站内还没有使用的电子信箱');
+            $this->get('session')->getFlashBag()->add('error','请使用4-36个英文字母或数字作为用户名,同时请使用网站内还没有使用的用户名和电子信箱');
+            return $param;
         }
-
-        return $param;
     }
 
     protected function getForm($arr)
