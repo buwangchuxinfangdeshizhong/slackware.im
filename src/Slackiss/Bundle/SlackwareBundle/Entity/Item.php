3ff1354a4207695fea9e697143fdd6cc24d722c9
Haulyn Jason <iam@saharabear.com>
2014-10-11 03:13:14 +0000

    generate Knowledge Pool Item Entity

---
+++ src/Slackiss/Bundle/SlackwareBundle/Entity/Item.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Slackiss\Bundle\SlackwareBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Item
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Slackiss\Bundle\SlackwareBundle\Entity\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="version", type="integer")
+     */
+    private $version;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="changelog", type="text")
+     */
+    private $changelog;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     * @return Item
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Item
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Item
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set version
+     *
+     * @param integer $version
+     * @return Item
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    /**
+     * Get version
+     *
+     * @return integer 
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Set changelog
+     *
+     * @param string $changelog
+     * @return Item
+     */
+    public function setChangelog($changelog)
+    {
+        $this->changelog = $changelog;
+
+        return $this;
+    }
+
+    /**
+     * Get changelog
+     *
+     * @return string 
+     */
+    public function getChangelog()
+    {
+        return $this->changelog;
+    }
+}
